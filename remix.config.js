5884a0e659acbc802f74e410bb439a4221d15ede
chriskg <christopher.k.gregory@gmail.com>
2023-11-19 12:34:44 -0600

    fix: added server file

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,27 @@
+import path from 'node:path'
+
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
-};
+  cacheDirectory: './node_modules/.cache/remix',
+  ignoredRouteFiles: ['**/.*', '**/*.test.{js,jsx,ts,tsx}'],
+  publicPath: '/_static/build/',
+  server: 'server.ts',
+  serverBuildPath: 'server/index.mjs',
+  serverModuleFormat: 'esm',
+  routes: (defineRoutes) =>
+    defineRoutes((route) => {
+      if (process.env.NODE_ENV === 'production') return
+
+      console.log('⚠️  Test routes enabled.')
+
+      const appDir = path.join(process.cwd(), 'app')
+
+      route(
+        '__tests/create-user',
+        path.relative(
+          appDir,
+          'cypress/support/test-routes/create-user.ts',
+        ),
+      )
+    }),
+}
