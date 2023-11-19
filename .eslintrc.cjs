0d076b1020783354a6c98a0a65b2c738ad084cab
chriskg <christopher.k.gregory@gmail.com>
2023-11-19 11:40:11 -0600

    fix: lint stage and git attributes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,8 +11,15 @@ module.exports = {
     "react/react-in-jsx-scope": "off",
     "react-hooks/exhaustive-deps": "warn",
     "react-hooks/rules-of-hooks": "error",
-    "@typescript-eslint/no-unsafe-return": 'off'
+    "@typescript-eslint/no-unsafe-return": 'off',
+    "jsx-a11y/anchor-is-valid": "off"
   },
+  ignorePatterns: [
+    "node_modules/",
+    "dist/",
+    "build/",
+    ".cache/"
+  ],
   settings: {
     react: {
       version: 'detect'
--- app/components/landing/ProjectCard.tsx
+++ app/components/landing/ProjectCard.tsx
@@ -10,7 +10,7 @@ export default function ProjectCard(
   const { image, title, description } = props
   return (
     <div className="space-y-4">
-      <img src={image} />
+      <img alt="placeholder project" src={image} />
       <div className="space-y-4">
         <h3 className="text-2xl text-zinc-100">{title}</h3>
         <p className="text-zinc-300">{description}</p>
